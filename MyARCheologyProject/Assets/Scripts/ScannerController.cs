b76db9c07af8ebda12acfea8c663373e1136ec1d
Pedro Larry <89364723+larry4us@users.noreply.github.com>
2026-01-17 16:25:04 -0400

    feat: adicionei o scanner que recebe um gameobject posicionado no seu centro

---
+++ MyARCheologyProject/Assets/Scripts/ScannerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScannerController : MonoBehaviour
+{
+
+    [SerializeField] private SpotController spot;
+    [SerializeField] private float scamDuration = 3f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();  
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {   
+        Debug.Log("Colidiu");
+        if (collision.transform.TryGetComponent<IInteractable>(out IInteractable interactable))
+        {
+            Debug.Log("Tentando colocar no spot");
+            TryToPutOnSpot(collision.gameObject);
+        }
+    }
+
+    private void TryToPutOnSpot(GameObject obj)
+    {
+        if (!spot.IsOccupied())
+        {
+            Debug.Log("Conseguiu colocar no spot");
+            obj.transform.SetParent(spot.transform);
+            obj.transform.localPosition = Vector3.zero;
+            obj.transform.localRotation = Quaternion.identity;
+
+            if (obj.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                rb.isKinematic = true;
+            }
+        }
+    }
+
+}
