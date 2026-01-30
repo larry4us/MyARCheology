846c7cb812d06441648c52c252ecef94d96ba6f6
larry <pedrolarry.jj@gmail.com>
2026-01-30 13:56:07 -0400

    feat: adicionei o efeito de particulas em todos os objetos sujos da cena.

--- MyARCheologyProject/Assets/Scripts/MaterialCleaner.cs
+++ MyARCheologyProject/Assets/Scripts/MaterialCleaner.cs
@@ -9,14 +9,28 @@ public class MaterialCleaner : MonoBehaviour
 
     [SerializeField] private MeshRenderer meshRenderer;
 
+    [SerializeField] private GameObject vfxDirtEffect;
+
     public void Clean()
     {
         if (newCleanMaterial != null && meshRenderer != null)
         {
             meshRenderer.material = newCleanMaterial;
+            removeDirtEffect();
         } else
         {
             Debug.LogWarning("Erro no material cleaner: algum dos parametros é null");
+        }
+    }
+
+    private void removeDirtEffect()
+    {
+        if (vfxDirtEffect != null)
+        {
+            vfxDirtEffect.SetActive(false);
+        } else
+        {
+            Debug.Log("O objeto não possuia Dirt Effect");
         }
     }
 }
